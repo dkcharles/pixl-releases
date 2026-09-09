fdfe85680b78e7d20aa0805b2705abebf8bc3ff0
Pixl publish <noreply@pixl>
2026-09-09 17:47:04 +0100

    Publish web player + Studio 2026-09-09 17:47

---
+++ play/_framework/dotnet.boot.js
@@ -0,0 +1,123 @@
+export const config = /*json-start*/{
+  "mainAssemblyName": "Pixl.Web.dll",
+  "resources": {
+    "hash": "sha256-mIP+rh2Trn7pb0VH9QQT1+hMc00Ry9t5Q++IciDvW2Y=",
+    "jsModuleNative": [
+      {
+        "name": "dotnet.native.js"
+      }
+    ],
+    "jsModuleRuntime": [
+      {
+        "name": "dotnet.runtime.js"
+      }
+    ],
+    "wasmNative": [
+      {
+        "name": "dotnet.native.wasm",
+        "hash": "sha256-wO/aDOHRaQ4iOtxcrxt/ApyaXQ2uU8WYyzvIVaTOVNQ="
+      }
+    ],
+    "wasmSymbols": [
+      {
+        "name": "dotnet.native.js.symbols"
+      }
+    ],
+    "coreAssembly": [
+      {
+        "virtualPath": "System.Private.CoreLib.wasm",
+        "name": "System.Private.CoreLib.wasm",
+        "hash": "sha256-PsvNdwSBj2CI8eOWja7QuGYEsevCyoyrRALpv2GpL0E="
+      },
+      {
+        "virtualPath": "System.Runtime.InteropServices.JavaScript.wasm",
+        "name": "System.Runtime.InteropServices.JavaScript.wasm",
+        "hash": "sha256-HxVMIUSAN3zLPzQqXVjqqLN42CtIfpKxp/4gNQfLHDo="
+      }
+    ],
+    "assembly": [
+      {
+        "virtualPath": "Pixl.Core.wasm",
+        "name": "Pixl.Core.wasm",
+        "hash": "sha256-ljCu4qfFQXeDpTiQZQjLSvm17RBaJF6+tnaxoOaFxy8="
+      },
+      {
+        "virtualPath": "Pixl.Studio.Core.wasm",
+        "name": "Pixl.Studio.Core.wasm",
+        "hash": "sha256-p+h/3mlTaoePTM1KRU+glydPDMpF/mMThwJz9MzFjFg="
+      },
+      {
+        "virtualPath": "Pixl.Web.wasm",
+        "name": "Pixl.Web.wasm",
+        "hash": "sha256-//qluz5uLxlFCg3c5QyAsREZRR3mtrIHIPTS+kLYElM="
+      },
+      {
+        "virtualPath": "System.Console.wasm",
+        "name": "System.Console.wasm",
+        "hash": "sha256-3fYDec1CnHnapxPKobzhJG2B6Eqx7JcSw7bmS9Nrdq8="
+      },
+      {
+        "virtualPath": "System.IO.Compression.wasm",
+        "name": "System.IO.Compression.wasm",
+        "hash": "sha256-+JAU1+Zg4Sm+krG9H95fQpgETGzUJu72MW+4SXnTQQs="
+      },
+      {
+        "virtualPath": "System.Linq.wasm",
+        "name": "System.Linq.wasm",
+        "hash": "sha256-hOpDRGy43ka0EpzHKRQP/hjN14geg27ymf9CMYKg3vs="
+      },
+      {
+        "virtualPath": "System.Memory.wasm",
+        "name": "System.Memory.wasm",
+        "hash": "sha256-/GVjye53uYnU9EKRAGQ1De3jCkooEznU/GzkwwCPw5w="
+      },
+      {
+        "virtualPath": "System.Text.Encodings.Web.wasm",
+        "name": "System.Text.Encodings.Web.wasm",
+        "hash": "sha256-TdR27QfW+93nkdGB8/NNZj4W13qc8arsoqwe+DWJe5s="
+      },
+      {
+        "virtualPath": "System.Text.Json.wasm",
+        "name": "System.Text.Json.wasm",
+        "hash": "sha256-z7vODMIo7A6sYhJ38EtkLCzQuoyJuluWb2nKbTDajdk="
+      }
+    ]
+  },
+  "debugLevel": 0,
+  "globalizationMode": "invariant",
+  "runtimeConfig": {
+    "runtimeOptions": {
+      "configProperties": {
+        "Microsoft.Extensions.DependencyInjection.VerifyOpenGenericServiceTrimmability": true,
+        "System.ComponentModel.DefaultValueAttribute.IsSupported": false,
+        "System.ComponentModel.Design.IDesignerHost.IsSupported": false,
+        "System.ComponentModel.TypeConverter.EnableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization": false,
+        "System.ComponentModel.TypeDescriptor.IsComObjectDescriptorSupported": false,
+        "System.Data.DataSet.XmlSerializationIsSupported": false,
+        "System.Diagnostics.Debugger.IsSupported": false,
+        "System.Diagnostics.Metrics.Meter.IsSupported": false,
+        "System.Diagnostics.Tracing.EventSource.IsSupported": false,
+        "System.Globalization.Invariant": true,
+        "System.TimeZoneInfo.Invariant": false,
+        "System.Globalization.PredefinedCulturesOnly": true,
+        "System.Linq.Enumerable.IsSizeOptimized": true,
+        "System.Net.Http.EnableActivityPropagation": false,
+        "System.Net.Http.WasmEnableStreamingResponse": true,
+        "System.Net.SocketsHttpHandler.Http3Support": false,
+        "System.Reflection.Metadata.MetadataUpdater.IsSupported": false,
+        "System.Resources.ResourceManager.AllowCustomResourceTypes": false,
+        "System.Resources.UseSystemResourceKeys": true,
+        "System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeSupported": true,
+        "System.Runtime.InteropServices.BuiltInComInterop.IsSupported": false,
+        "System.Runtime.InteropServices.EnableConsumingManagedCodeFromNativeHosting": false,
+        "System.Runtime.InteropServices.EnableCppCLIHostActivation": false,
+        "System.Runtime.InteropServices.Marshalling.EnableGeneratedComInterfaceComImportInterop": false,
+        "System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization": false,
+        "System.StartupHookProvider.IsSupported": false,
+        "System.Text.Encoding.EnableUnsafeUTF7Encoding": false,
+        "System.Text.Json.JsonSerializer.IsReflectionEnabledByDefault": false,
+        "System.Threading.Thread.EnableAutoreleasePool": false
+      }
+    }
+  }
+}/*json-end*/;
---
+++ studio/js/pixl-editor.js
@@ -0,0 +1,12 @@
+import { e as s, d as a, f as r, a as o, g as d, r as i, h as g, b as E, s as c } from "./index-CrUdZmNK.js";
+export {
+  s as createEditor,
+  a as destroyEditor,
+  r as focusEditorAt,
+  o as getEditorText,
+  d as getSelection,
+  i as replaceRange,
+  g as setEditorAppearance,
+  E as setEditorText,
+  c as setSquiggles
+};
